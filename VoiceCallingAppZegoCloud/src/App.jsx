d2f3d675f74ebacc53674a19ff1362ef22e7f91e
Deepak Mallareddy <deepak8519m@gmail.com>
2025-06-25 01:43:44 +0530

    mostly final code

--- VoiceCallingAppZegoCloud/src/App.jsx
+++ VoiceCallingAppZegoCloud/src/App.jsx
@@ -25,15 +25,15 @@ function App() {
     zp.addPlugins({ ZIM });
   }, [TOKEN]);
 
-  function invite() {
+  function invite(callType) {
     const targetUser = {
-      userID: prompt("Enter Callee's userId: "),
-      userName: prompt("Enter Callee's userName: "),
+      userID: prompt("Enter Callee's userId : "),
+      userName: prompt("Enter Callee's userName : "),
     };
     zpRef.current
       .sendCallInvitation({
         callees: [targetUser],
-        callType: ZegoUIKitPrebuilt.InvitationTypeVideoCall,
+        callType,
         timeout: 60, // Timeout duration (second). 60s by default, range from [1-600s].
       })
       .then((res) => {
@@ -44,7 +44,34 @@ function App() {
       });
   }
 
-  return <div></div>;
+  return (
+    <div className="w-full h-screen bg-linear-to-b from-[#1a2229] to-black flex items-center justify-center ">
+      <div className="w-[600px] h-[500px] bg-[#0d1014] border-3 border-[#313030] rounded-xl flex flex-col items-center justify-center gap-10">
+        <h2 className="text-white text-[20px]">
+          <span className="text-blue-500">User Name : </span>
+          {userName}
+        </h2>
+
+        <h2 className="text-white text-[20px]">
+          <span className="text-blue-500">User Id : </span>
+          {userID}
+        </h2>
+
+        <button
+          className="w-[200px] h-[50px] rounded-[4px] cursor-pointer border-transparent bg-white text-black text-[20px]"
+          onClick={() => invite(ZegoUIKitPrebuilt.InvitationTypeVoiceCall)}
+        >
+          Voice Call
+        </button>
+        <button
+          className="w-[200px] h-[50px] rounded-[4px] cursor-pointer border-transparent bg-white text-black text-[20px]"
+          onClick={() => invite(ZegoUIKitPrebuilt.InvitationTypeVideoCall)}
+        >
+          Video Call
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
