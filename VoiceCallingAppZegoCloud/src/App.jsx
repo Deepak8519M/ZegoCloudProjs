028e478ea968fe4cff223a911c0e132f6c455634
Deepak Mallareddy <deepak8519m@gmail.com>
2025-06-25 01:02:23 +0530

    doc starting instructions added

--- VoiceCallingAppZegoCloud/src/App.jsx
+++ VoiceCallingAppZegoCloud/src/App.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+
+import { ZIM } from "zego-zim-web";
+import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
 
 function App() {
-  return (
-    <div>
-      
-    </div>
-  )
+  const userID = "user" + Math.floor(Math.random() * 1000);
+  const userName = "userName" + userID;
+  const appID = 638367256;
+  const serverSecret = "0431c19bb72748171026c520012b7a1a";
+  return <div></div>;
 }
 
-export default App
+export default App;
